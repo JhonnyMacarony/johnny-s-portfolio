43763c211876eafa3a7f0fcdedf3772ae71e434c
JhonnyMacarony <91407912+JhonnyMacarony@users.noreply.github.com>
2022-03-20 10:46:55 +0200

    first commit

--- src/projects/CommunityPage.js
+++ src/projects/CommunityPage.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const CommunityPage = () => {
   return (
-    <a href='https://comunity-page.herokuapp.com/' target="_blank">
+    <a href='https://comunity-page.herokuapp.com/' rel="noreferrer" target="_blank">
         CommunityPage
     </a>
   )
